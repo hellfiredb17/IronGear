838d8059ab80514e0951c4463192bc6d3c5a8c1a
Surface <duncanbudingerjr@gmail.com>
2020-04-30 23:10:02 -0700

    Board editor pan control and zoom

---
+++ Assets/Scripts/Preferences/BoardEditorPreferences.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "IronGears/BoardEditorPreference")]
+public class BoardEditorPreferences : ScriptableObject
+{
+    //---- Input Controls
+    //-------------------
+    [Header("Input Controls")]
+
+    public Vector2 ZoomClamp = new Vector2(1, 30);
+    public float DragThreshold = 1.0f;
+    public float ZoomInterval = 5.0f;
+    public float CameraSpeed = 10.0f;
+}
--- Assets/Scripts/Tools/BoardEditorInputController.cs
+++ Assets/Scripts/Tools/BoardEditorInputController.cs
@@ -7,9 +7,14 @@ public class BoardEditorInputController : MonoBehaviour
 {
     //---- Variables
     //--------------
+    [SerializeField] private BoardEditorPreferences _preferences;
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private GameObject _mouse;
+    [SerializeField] private Transform _cameraTarget;
     [SerializeField] private BoardEditorTool _tool;
+
+    private float _zoomValue;
+    private Vector3 _lastPosition;
 
     private HexTileModel _currentModel;
     private HexTile _currentHover;
@@ -19,6 +24,12 @@ public class BoardEditorInputController : MonoBehaviour
 
     //---- Unity
     //----------
+    private void Awake()
+    {
+        _zoomValue = _mainCamera.orthographicSize;
+        _cameraTarget.position = _mainCamera.transform.position;
+    }
+
     private void FixedUpdate()
     {
         HexTile current = Raycast();
@@ -54,10 +65,14 @@ public class BoardEditorInputController : MonoBehaviour
             _tool.UpdateTile(_lastConvert, _currentModel);
         }
 
+        HandleKeyboardInput();
+
         // Right button down
-        if(!_rightBtnDown && Input.GetMouseButtonDown(1))
+        if (!_rightBtnDown && Input.GetMouseButtonDown(1))
         {
             _rightBtnDown = true;
+            _lastPosition = Input.mousePosition;
+            _lastPosition.z = _mainCamera.transform.position.y;
         }
         else if (_rightBtnDown && Input.GetMouseButtonUp(1))
         {
@@ -66,7 +81,22 @@ public class BoardEditorInputController : MonoBehaviour
 
         if(_rightBtnDown)
         {
-            // Handle pan camera
+            PanCamera();
+        }
+        
+        LerpToTarget();
+    }
+
+    private void HandleKeyboardInput()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ZoomIn();
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ZoomOut();
         }
     }
 
@@ -80,10 +110,41 @@ public class BoardEditorInputController : MonoBehaviour
 
     //---- Private
     //------------
-    private void HandlePanCamera()
+    private void LerpToTarget()
     {
-        // TODO - move camera base on mouse movement;
-        // Maybe just lerp to mouse position?
+        _mainCamera.transform.position = Vector3.Lerp(_mainCamera.transform.position, _cameraTarget.position, 
+            Time.fixedDeltaTime * _preferences.CameraSpeed);
+    }
+
+    private void PanCamera()
+    {
+        Vector3 lastPos = _mainCamera.ScreenToWorldPoint(_lastPosition);
+        lastPos.y = _mainCamera.transform.position.y;
+
+        Vector3 currentPos = Input.mousePosition;
+        currentPos.z = _mainCamera.transform.position.y;
+        currentPos = _mainCamera.ScreenToWorldPoint(currentPos);
+        currentPos.y = _mainCamera.transform.position.y;
+
+        Vector3 delta = currentPos - lastPos;
+        _cameraTarget.position += delta;
+
+        _lastPosition = Input.mousePosition;
+        _lastPosition.z = _mainCamera.transform.position.y;
+    }
+
+    private void ZoomOut()
+    {
+        _zoomValue += _preferences.ZoomInterval;
+        _zoomValue = Mathf.Clamp(_zoomValue, _preferences.ZoomClamp.x, _preferences.ZoomClamp.y);
+        _mainCamera.orthographicSize = _zoomValue;
+    }
+
+    private void ZoomIn()
+    {
+        _zoomValue -= _preferences.ZoomInterval;
+        _zoomValue = Mathf.Clamp(_zoomValue, _preferences.ZoomClamp.x, _preferences.ZoomClamp.y);
+        _mainCamera.orthographicSize = _zoomValue;
     }
 
     private HexTile Raycast()
